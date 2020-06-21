a1f969fd5bc990781fe098308302529e2b749cdc
pedrol3001 <pedrolcr@outlook.com>
2020-06-21 15:28:09 -0300

    Build Pack First Test

---
+++ example/android/app/src/main/kotlin/com/example/pedrol_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pedrol_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/android/app/src/main/kotlin/com/example/pedrolcr_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pedrolcr_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
